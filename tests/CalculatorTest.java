b18fc98de4e4168dd6b93ec16197fedd794bbbae
ThomasDufour19 <dufourthomas159@gmail.com>
2025-05-05 12:24:59 +0200

    Premier Commit du projet java CI-CD

---
+++ tests/CalculatorTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+ 
+public class CalculatorTest {
+    @Test
+    public void testAddition() {
+        assertEquals(6, Calculator.add(3, 3));
+    }
+}
